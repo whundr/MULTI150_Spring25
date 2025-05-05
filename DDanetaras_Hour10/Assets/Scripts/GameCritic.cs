fd7fa1f1fba059108535a4587a4b0ea7acda34ca
whundr <71905827+whundr@users.noreply.github.com>
2025-05-04 17:06:15 -0700

    added gamecritic

---
+++ DDanetaras_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,7 @@
+/*
+ Four suggestions included in this game
+ 1. adding variations to the Chaos Ball
+ 2. changing the size and shape of the player bumper
+ 3. covering up the water with a terrain, plane, or other game objects around the border
+ 4. changing the footsteps to something that makes more sence with the water.
+ */
