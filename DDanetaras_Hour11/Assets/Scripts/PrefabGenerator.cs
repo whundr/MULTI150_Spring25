22d67575961aed3f2ee3415ab4e4c94d71a123e6
whundr <71905827+whundr@users.noreply.github.com>
2025-05-09 10:24:05 -0700

    finished code

--- DDanetaras_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ DDanetaras_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,10 +6,15 @@ public class PrefabGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject prefab;
-
+    public Vector3 spawnRange;
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRange.x, spawnRange.x), 0f,
+                Random.Range(-spawnRange.z, spawnRange.z));
+            Instantiate<GameObject>(prefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
