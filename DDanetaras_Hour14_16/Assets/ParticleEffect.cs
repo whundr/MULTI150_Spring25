c935576c75d89e649bc9e86aa5de68e56211866e
whundr <71905827+whundr@users.noreply.github.com>
2025-05-11 23:15:15 -0700

    Hour 16 Material

---
+++ DDanetaras_Hour14_16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+ 1. Start Speed as a curve modulating from fast to slow.
+ 2. Start Size as a curve modulating from big to small particles.
+ 3. Start Color is a gradient of red, yellow, and orange.
+ 4. 3D Start rotation curves on the X,Y, and Z axis.
+ 5. Burst Shape to Sphere
+ */
--- DDanetaras_Hour14_16/Assets/record the modifications.cs
+++ DDanetaras_Hour14_16/Assets/record the modifications.cs
@@ -1,5 +1,5 @@
 /*
- List of modifications:
+ List of modifications: For Hour 14
 1. Changed the contents of the title and sub text box
 2. Changed background picture
 3. Changed font and size to fit the theme of the background
